d52771681ff3a1d236c8ebbbb09a93361ce38cac
Deivison Gonçalves <97059867+Deivisongs@users.noreply.github.com>
2025-11-15 11:16:13 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ window.OPERATING_HOURS = {
     3: { open: "18:00", close: "23:00" }, // Quarta
     4: { open: "18:00", close: "23:00" }, // Quinta
     5: { open: "09:00", close: "23:59" }, // Sexta
-    6: { open: "18:00", close: "23:59" }, // Sábado
+    6: { open: "10:00", close: "23:59" }, // Sábado
     0: { open: "18:00", close: "23:00" }, // Domingo
     // Se quiser fechar em um dia, use: 1: { open: null, close: null }, 
 }; 
@@ -41,6 +41,24 @@ window.PRODUCTS = {
             price: 32.50, // <--- PRODUTO COM PREÇO NORMAL
             description: "Pão, blend de 180g, bacon crocante, cheddar cremoso e cebola caramelizada",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThRRWcrxrm1dKGMSluf_2Nnz9zglGuDJiNqA&s" 
+        },
+        {
+            name: "X-EGG",
+            price: 32.50, // <--- PRODUTO COM PREÇO NORMAL
+            description: "Pão, blend de 180g, bacon crocante, cheddar cremoso e cebola caramelizada",
+            image: "https://sachefmio.blob.core.windows.net/fotos/x-egg-73519.jpg" 
+        },
+        {
+            name: "X-Tudo",
+            price: 32.50, // <--- PRODUTO COM PREÇO NORMAL
+            description: "Pão, blend de 180g, bacon crocante, cheddar cremoso e cebola caramelizada",
+            image: "https://lanchessupermania.com.br/img/67ab4ba77fe9a.jpg" 
+        },
+        {
+            name: "X-Salada",
+            price: 32.50, // <--- PRODUTO COM PREÇO NORMAL
+            description: "Pão, blend de 180g, bacon crocante, cheddar cremoso e cebola caramelizada",
+            image: "https://t3.ftcdn.net/jpg/12/98/93/56/360_F_1298935654_5jgCT0UpdsWo2VV6f9aBXGKsFftGBDch.jpg" 
         }
     ],
     "Pizzas": [
@@ -70,6 +88,50 @@ window.PRODUCTS = {
             price: 8.50,
             description: "Suco natural de Uva fresquinho, sem conservantes",
             image: "https://wx.mlcdn.com.br/ponzi/production/portaldalu/58836.jpg"
+        },
+        {
+            name: "Suco Prats Laranja 300ml",
+            price: 8.50,
+            description: "Suco Prats de laranja",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/dd68c431-f3a1-450a-aca7-b69c4d0b4847_20250701225557.webp"
+        }
+    ],
+    "Refrigerantes": [
+        {
+            name: "Coca Lata 220ml",
+            price: 6.90,
+            description: "Coca Lata 220ml Original",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/627209c0-8ebe-42c2-9f8d-5eeda2c48305_20250701225550.webp"
+        },
+        {
+            name: "Guarana Antarctica 350ml",
+            price: 8.90,
+            description: "Guarana Antarctica 350ml",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/22c7d0ce-4795-4341-8ac5-0327d6737b06_20250701225553.webp"
+        },
+        {
+            name: "Coca Lata 220ml Zero",
+            price: 6.90,
+            description: "Coca Cola Zero 220ml",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/dbf6893f-4845-4f5d-9f66-e474b16e7fac_20250701225551.webp"
+        },
+        {
+            name: "Sprite Original 350ml",
+            price: 8.90,
+            description: "Sprite Original 350ml",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/75b02c96-a5f4-410a-94fb-ec415264e0cb_20250701225554.webp"
+        },
+        {
+            name: "Coca Cola KS 250ml",
+            price: 8.90,
+            description: "Coca Cola KS 250ml",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/ae1b4323-5b59-4b56-993e-095ca882482f_20250701225550.webp"
+        },
+        {
+            name: "Coca-Cola KS 250ml Zero",
+            price: 8.90,
+            description: "Coca-Cola KS 250ml Zero",
+            image: "https://files.menudino.com/merchants/c66216f7-2faf-11ee-9964-0022483864db/items/images/small/8a126704-b074-450a-9bdf-6ea44fa48066_20250701225550.webp"
         }
     ]
 };
